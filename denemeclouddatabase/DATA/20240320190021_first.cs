1d521c6ce9c44a64eb84a1eb1252cdb613c1a923
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2024-03-20 22:12:36 +0300

    Add project files.

---
+++ denemeclouddatabase/DATA/20240320190021_first.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace denemeclouddatabase.DATA
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Age", "Name", "Surname" },
+                values: new object[,]
+                {
+                    { 1, 30, "John", "Doe" },
+                    { 2, 25, "Jane", "Smith" },
+                    { 3, 35, "Michael", "Johnson" },
+                    { 4, 28, "Emily", "Brown" },
+                    { 5, 32, "James", "Wilson" },
+                    { 6, 29, "Emma", "Jones" },
+                    { 7, 31, "William", "Taylor" },
+                    { 8, 27, "Olivia", "Anderson" },
+                    { 9, 33, "Alexander", "Thomas" },
+                    { 10, 26, "Sophia", "Martinez" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ denemeclouddatabase/DATA/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace denemeclouddatabase.DATA
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed data ekleme
+            modelBuilder.Entity<Employee>().HasData(
+
+                 new Employee { Id = 1, Name = "John", Surname = "Doe", Age = 30 },
+                 new Employee { Id = 2, Name = "Jane", Surname = "Smith", Age = 25 },
+                 new Employee { Id = 3, Name = "Michael", Surname = "Johnson", Age = 35 },
+                 new Employee { Id = 4, Name = "Emily", Surname = "Brown", Age = 28 },
+                 new Employee { Id = 5, Name = "James", Surname = "Wilson", Age = 32 },
+                 new Employee { Id = 6, Name = "Emma", Surname = "Jones", Age = 29 },
+                 new Employee { Id = 7, Name = "William", Surname = "Taylor", Age = 31 },
+                 new Employee { Id = 8, Name = "Olivia", Surname = "Anderson", Age = 27 },
+                 new Employee { Id = 9, Name = "Alexander", Surname = "Thomas", Age = 33 },
+                 new Employee { Id = 10, Name = "Sophia", Surname = "Martinez", Age = 26 }
+                 );
+        }
+    }
+}
+
+
+
+
